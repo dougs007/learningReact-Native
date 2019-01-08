0b0966d998e7c3b8358c077b0ab0fcd3b621255f
Douglas <douglasantana007@gmail.com>
2019-01-08 17:43:08 -0200

    [UPDATE] Aula 1.
    Criado primeiro componente no react native.

--- exercicios/App.js
+++ exercicios/App.js
@@ -6,44 +6,27 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, StyleSheet} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'I hope you liked the project. you like this project!\n' +
-    'Hello World using React Native ! yeahh ;)',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>Developed by: Douglas x</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+export default class App extends React.Component {
+    render() {
+        return (
+          <View style={styles.container}>
+              <Text style={styles.f20}>App com Hello World!</Text>
+          </View>
+        )
+    }
 }
 
+// Constantes de estilos.
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    f20: {
+        fontSize: 40
+    }
+})
