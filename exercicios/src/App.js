9d8822275d101a56f9dcddd40db2f051adbfa425
Douglas <douglasantana007@gmail.com>
2019-01-08 23:03:06 -0200

    [ADD] Import/Export.
    Finalizada aula 22, entendendo mais sobre import e export. Criado componentes para inverter texto e outro de gerar números da MegaSena.

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -3,13 +3,18 @@ import { View, StyleSheet } from 'react-native'
 
 import Simples from './componentes/Simples'
 import ParImpar from './componentes/ParImpar'
+import { Inverter, MegaSena } from './componentes/Multi'
+import Sobre from './componentes/Sobre'
 
 export default class App extends React.Component {
 	render() {
 		return (
 			<View style={styles.container}>
+                <Sobre about='App with React Native!'></Sobre>
 				<Simples texto='Flexível!!!!' />
 				<ParImpar numero={150} />
+                <Inverter texto='React Native !' />
+                <MegaSena numeros={10} />
 			</View>
 		)
 	}
@@ -20,5 +25,5 @@ const styles = StyleSheet.create({
 		flex: 1,
 		justifyContent: 'center',
 		alignItems: 'center',
-	}
+    }
 })
