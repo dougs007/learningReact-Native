0233e821bb7985805547dbc27b7867dc8aff7f1f
Douglas <douglasantana007@gmail.com>
2019-01-09 20:35:11 -0200

    [ADD] App Calculadora.
    Criada estruturação dos arquivos de componentes do projeto, criado arquivo de botão. Finalizada aula 36 e 37

--- AppCalculator/App.js
+++ AppCalculator/App.js
@@ -1,32 +1,35 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, {Component} from 'react'
+import {Platform, StyleSheet, Text, View} from 'react-native'
+import Button from './src/components/Button'
+// import Display from './src/components/Display'
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+export default class App extends Component {
+    render() {
+        return (
+            <View style={ styles.container }>
+                {/* <Display value={ this.state.displayValue } /> */}
+                <View style={ styles.buttons }>
+                    <Button label='AC' triple onClick={this.clearMemory} />
+                    <Button label='/' operation onClick={this.setOperation} />
+                    <Button label='7' onClick={this.addDigit} />
+                    <Button label='8' onClick={this.addDigit} />
+                    <Button label='9' onClick={this.addDigit} />
+                    <Button label='*' operation  onClick={this.setOperation} />
+                    <Button label='4' onClick={this.addDigit} />
+                    <Button label='5' onClick={this.addDigit} />
+                    <Button label='6' onClick={this.addDigit} />
+                    <Button label='-' operation onClick={this.setOperation} />
+                    <Button label='1' onClick={this.addDigit} />
+                    <Button label='2' onClick={this.addDigit} />
+                    <Button label='3' onClick={this.addDigit} />
+                    <Button label='+' operation onClick={this.setOperation} />
+                    <Button label='0' double  onClick={this.addDigit} />
+                    <Button label='.' onClick={this.addDigit} />
+                    <Button label='=' operation onClick={this.setOperation} />
+                </View>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
@@ -34,16 +37,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+  buttons: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  }
+})
