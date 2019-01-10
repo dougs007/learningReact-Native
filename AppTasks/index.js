e53cd020d02dba2aa9cb48dab463069d99412a11
Douglas <douglasantana007@gmail.com>
2019-01-10 15:56:16 -0200

    [ADD/DELETE] App Tasks.
    Implementado view do app Tarefas, removido o file App.js. Finalizada aula 64.

--- AppTasks/index.js
+++ AppTasks/index.js
@@ -1,7 +1,5 @@
-/** @format */
+import { AppRegistry } from 'react-native';
+import Agenda from './src/screens/Agenda';
+import { name as appName } from './app.json';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Agenda);
