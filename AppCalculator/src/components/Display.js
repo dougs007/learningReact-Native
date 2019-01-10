c3b522cecba53b241c47068c0785aa50b0709c8c
Douglas <douglasantana007@gmail.com>
2019-01-10 00:19:02 -0200

    [ADD/FIX] App Calculadora.
    Implementado app calculadora, finalizado app.

--- AppCalculator/src/components/Display.js
+++ AppCalculator/src/components/Display.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Text, View, } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 
 const styles = StyleSheet.create({
     display: {
@@ -15,7 +15,7 @@ const styles = StyleSheet.create({
     }
 })
 
-export default props => 
+export default props =>
     <View style={ styles.display }>
         <Text style={ styles.displayValue }
             numberOfLines={1}>{ props.value }</Text>
