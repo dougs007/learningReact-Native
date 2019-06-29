fe3614fccb8d6150448eedcf4f0240ba6e54e0bb
Douglasx <douglasantana007@gmail.com>
2019-06-29 01:57:40 -0300

    [ADD] Tasks Mobile.
    Adicionada aplicação tasks em ReactNative.

--- AppTasks/index.js
+++
@@ -1,5 +0,0 @@
-import { AppRegistry } from 'react-native';
-import Agenda from './src/screens/Agenda';
-import { name as appName } from './app.json';
-
-AppRegistry.registerComponent(appName, () => Agenda);
